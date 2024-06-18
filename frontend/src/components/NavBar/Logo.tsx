7668ee49fb7bff1b6742936a60310036aa1daf6c
MarceloHcb <marcelohcb@msn.com>
2024-06-17 22:26:58 -0300

    up

--- frontend/src/components/NavBar/Logo.tsx
+++ frontend/src/components/NavBar/Logo.tsx
@@ -7,7 +7,7 @@ export function Logo() {
     justify-center gap-1 md:gap-4 flex-shrink-0"
     >
       <Image width={ 35 } height={ 35 } src="/images/logo.png" alt="Note" />
-      <h2>McloNotes</h2>
+      <h2>CoreNotes</h2>
     </div>
   );
 }
